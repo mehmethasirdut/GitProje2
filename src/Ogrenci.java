bfad7bcf406d3ea0e89aa3781c4dac1d8b38f5a0
avhas <av.hasirdut@gmail.com>
2022-07-14 18:09:14 +0300

    Ogrenci Classı ikinci bölüm yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,9 @@
 public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("Hello Jawa");
+
+        System.out.println("Merhaba dünya 2");
+
+        System.out.println("Merhaba dünya 3");
     }
 }
